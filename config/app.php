99db251b24d96bba65acd62f5cc4aa3379eb852c
osaidhaj <osaidhaj03@gmail.com>
2025-10-16 21:50:30 +0300

    refactor(config): use env variable for timezone configuration
    Make timezone configurable through environment variable while keeping 'Asia/Amman' as fallback value

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Amman',
+    'timezone' => env('APP_TIMEZONE', 'Asia/Amman'),
 
     /*
     |--------------------------------------------------------------------------
