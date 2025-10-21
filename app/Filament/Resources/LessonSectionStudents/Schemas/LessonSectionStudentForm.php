cbe95b11b28e06d0b78e258bcda08f24a77dff9c
osaidhaj <osaidhaj03@gmail.com>
2025-10-21 18:41:29 +0300

    refactor: update import path for Section component in form schema

--- app/Filament/Resources/LessonSectionStudents/Schemas/LessonSectionStudentForm.php
+++ app/Filament/Resources/LessonSectionStudents/Schemas/LessonSectionStudentForm.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\LessonSectionStudents\Schemas;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use App\Models\LessonSection;
 use App\Models\User;
