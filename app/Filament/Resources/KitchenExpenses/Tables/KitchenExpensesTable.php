d26329f6813ef32026a32ee0ec85ca07f1b00060
osaidhaj <osaidhaj03@gmail.com>
2025-12-27 23:31:08 +0300

    feat: add Filament table for Kitchen Expenses with columns, filters, and actions

--- app/Filament/Resources/KitchenExpenses/Tables/KitchenExpensesTable.php
+++ app/Filament/Resources/KitchenExpenses/Tables/KitchenExpensesTable.php
@@ -7,6 +7,7 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Filament\Tables\Filters\Filter;
 
 class KitchenExpensesTable
 {
