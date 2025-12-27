91bc01af5f7259d35f6de0060e23ff5884e051b8
osaidhaj <osaidhaj03@gmail.com>
2025-12-27 23:47:27 +0300

    feat: Add Filament Student panel provider including authentication, dashboard, and plugins.

--- app/Providers/Filament/StudentPanelProvider.php
+++ app/Providers/Filament/StudentPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Widgets\AccountWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -17,6 +18,7 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use App\Filament\Student\Pages\StudentDashboard;
 use Jeffgreco13\FilamentBreezy\BreezyCore;
+use Caresome\FilamentNeobrutalism\NeobrutalismeTheme;
 
 class StudentPanelProvider extends PanelProvider
 {
@@ -26,15 +28,28 @@ class StudentPanelProvider extends PanelProvider
             ->id('student')
             ->path('student')
             ->login()
+            ->brandName('لوحة التحكم الخاصة بالطالب')
             ->colors([
-                'primary' => Color::Blue,
+                'primary' => Color::Indigo,
+            ])
+            ->plugins([
+                NeobrutalismeTheme::make(),
+                BreezyCore::make()
+                    ->myProfile(
+                        shouldRegisterUserMenu: true,
+                        shouldRegisterNavigation: false,
+                        hasAvatars: true,
+                        slug: 'my-profile'
+                    ),
             ])
             ->pages([
                 StudentDashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Student/Widgets'), for: 'App\\Filament\\Student\\Widgets')
+            ->widgets([
+                AccountWidget::class,
+            ])
             ->registration(\App\Filament\Student\Pages\Auth\Register::class)
-            ->plugin(BreezyCore::make())
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
