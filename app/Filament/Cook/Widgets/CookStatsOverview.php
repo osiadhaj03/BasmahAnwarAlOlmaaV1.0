d82f175f3ab7319c8874b28b3c97ce23fb18b4ba
osaidhaj03 <osaidhaj03@gmail.com>
2025-12-28 13:35:25 +0300

    feat: add new Filament Cook panel provider

--- app/Filament/Cook/Widgets/CookStatsOverview.php
+++ app/Filament/Cook/Widgets/CookStatsOverview.php
@@ -12,7 +12,7 @@ use Carbon\Carbon;
 
 class CookStatsOverview extends BaseWidget
 {
-    protected static ?int $sort = 1;
+    //protected static ?int $sort = 1;
     
     protected function getStats(): array
     {
