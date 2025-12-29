5e13f0e5a23a6773d25938f45c0cfdfbd4e34ed6
osaidhaj <osaidhaj03@gmail.com>
2025-12-29 21:03:03 +0300

    feat: Add CreateKitchenInvoice page

--- app/Filament/Resources/KitchenInvoices/Pages/CreateKitchenInvoice.php
+++ app/Filament/Resources/KitchenInvoices/Pages/CreateKitchenInvoice.php
@@ -3,9 +3,25 @@
 namespace App\Filament\Resources\KitchenInvoices\Pages;
 
 use App\Filament\Resources\KitchenInvoices\KitchenInvoiceResource;
+use App\Models\KitchenInvoice;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateKitchenInvoice extends CreateRecord
 {
     protected static string $resource = KitchenInvoiceResource::class;
+
+    /**
+     * توليد رقم الفاتورة عند الحفظ لتجنب التكرار
+     */
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // توليد رقم فاتورة جديد عند الحفظ (وليس عند فتح النموذج)
+        $data['invoice_number'] = KitchenInvoice::generateInvoiceNumber();
+        
+        // إزالة الحقول غير الموجودة في الجدول
+        unset($data['user_id_selector']);
+        unset($data['subscription_number_display']);
+        
+        return $data;
+    }
 }
